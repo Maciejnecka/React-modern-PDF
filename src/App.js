4d9b94e64644a6385dec9ae5d1f4f3f6e1e299d4
Maciej Necka <macieknecka@gmail.com>
2023-11-28 14:52:27 +0100

    #03 useRef()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import './styles/styles.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  const [isHover, setIsHover] = useState(false);
-  const style = { border: `${isHover * 4}px solid red` };
+  const [entered, setEntered] = useState(0);
+
+  // console.log('before');
+  // useEffect(() => {
+  //   console.log('counter: ', counter);
+  // }, [counter]);
+  // console.log('after');
+
+  // useEffect(() => {
+  //   document.title = `${counter} / ${entered}`;
+  // }, [counter, entered]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCounter((value) => value + 1);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
   return (
-    <div>
-      <button
-        style={style}
-        onMouseEnter={(e) => setIsHover(true)}
-        onMouseLeave={(e) => setIsHover(false)}
-        onClick={(e) => setCounter(counter + 1)}
-      >
-        {counter}
-      </button>
-    </div>
+    <div>{counter}</div>
+    // <div>
+    //   <button
+    //     onMouseEnter={(e) => setEntered(entered + 1)}
+    //     onClick={(e) => setCounter(counter + 1)}
+    //   >
+    //     {counter} / {entered}
+    //   </button>
+    // </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import TextContext from './context/TextContext';
-import ColorContext from './context/ColorContext';
-
-export { TextContext, ColorContext };
-
 const root = createRoot(document.querySelector('#root'));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
